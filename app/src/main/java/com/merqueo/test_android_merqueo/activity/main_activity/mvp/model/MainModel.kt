ac77c0d930ac726cb2b7a186357b734ed42081d9
Armando Chacon <jesus.chacon@quick.com.co>
2019-09-25 21:42:17 -0500

    add model of calls to the api through the presenter to obtain asynchronously the list of movies

--- app/src/main/java/com/merqueo/test_android_merqueo/activity/main_activity/mvp/model/MainModel.kt
+++ app/src/main/java/com/merqueo/test_android_merqueo/activity/main_activity/mvp/model/MainModel.kt
@@ -1,6 +1,14 @@
 package com.merqueo.test_android_merqueo.activity.main_activity.mvp.model
 
 import com.merqueo.test_android_merqueo.dagger.network.Network
+import io.reactivex.Observable
+import retrofit2.Response
 
-class MainModel(network: Network) {
+class MainModel(private val network: Network) {
+
+    fun getMovies(api_key: String, page: Int): Observable<Response<String>> {
+        return network.postLogin(api_key,page)!!
+    }
+
+
 }
--- app/src/main/java/com/merqueo/test_android_merqueo/activity/main_activity/mvp/presenter/MainPresenter.kt
+++ app/src/main/java/com/merqueo/test_android_merqueo/activity/main_activity/mvp/presenter/MainPresenter.kt
@@ -1,10 +1,64 @@
 package com.merqueo.test_android_merqueo.activity.main_activity.mvp.presenter
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.merqueo.test_android_merqueo.activity.data_model.Movies_model
 import com.merqueo.test_android_merqueo.activity.main_activity.mvp.model.MainModel
 import com.merqueo.test_android_merqueo.activity.main_activity.mvp.view.MainView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.realm.Realm
 
 class MainPresenter(
-    view: MainView,
-    model: MainModel
+    private val view: MainView,
+    private val model: MainModel
 ) {
+    private val realm = Realm.getDefaultInstance()
+
+    fun getMovies(api_key : String,page : Int): Disposable {
+        return model.getMovies(api_key, page)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({ response ->
+                val code: Int = response.code()
+                when (code) {
+                    200 -> {
+
+                        val movies: Movies_model = gsonBuilder().fromJson(response.body(), Movies_model::class.java)
+                        realm.executeTransaction { realm -> realm.insertOrUpdate(movies) }
+
+                    }
+
+                    401 -> {
+
+                    }
+
+                    422 -> {
+
+                    }
+
+                    500 -> {
+
+                    }
+                    else -> {
+
+                    }
+                }
+
+
+            }, {
+
+            })
+    }
+
+    private fun gsonBuilder(): Gson {
+
+        lateinit var gson: Gson
+        val gsonBuilder = GsonBuilder()
+        gsonBuilder.setDateFormat("M/d/yy hh:mm a")
+        gson = gsonBuilder.create()
+
+        return gson
+    }
 }
